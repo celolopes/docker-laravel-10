c9b443686231ce9b85609b3d56d0a2f89c72ace2
Marcelo Lopes <marcelo@sc.senac.br>
2024-05-15 17:20:11 -0300

    frontend

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -109,6 +109,7 @@
 </head>
 
 <body>
+    @include('components.bttheme')
     <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
         <symbol id="calendar3" viewBox="0 0 16 16">
             <path
