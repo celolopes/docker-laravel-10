5ce344635bbacd90c7314520f206b5b2c3f74799
Marcelo Lopes <marcelo@sc.senac.br>
2024-05-16 14:01:08 -0300

    up

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutosController extends Controller
 {
     //
     public function index() {
-        return view('produtos.index');
+        $produtos = Produto::all();
+        return view('produtos.index', compact('produtos'));
     }
 }
